b775b25da3d4cc22b1c016e215154d5e67e31274
seemab khalil <seemab@interactivewings.com>
2019-11-14 00:41:55 +0500

    added

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,5 +1,5 @@
-// Copyright IBM Corp. 2016. All Rights Reserved.
-// Node module: loopback-workspace
+// Copyright IBM Corp. 2014,2016. All Rights Reserved.
+// Node module: loopback-example-passport
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
@@ -8,6 +8,6 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/status', server.loopback.status());
   server.use(router);
 };
